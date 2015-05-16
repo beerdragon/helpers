2e7f47f68be458f84fe9c458f86f8338ccdd91cf
Andrew Griffin <aiwg@beerdragon.co.uk>
2015-05-16 22:32:34 +0100

    Allow CI server to specify a non-default drop folder for website content.

--- junit/ci-post.sh
+++ junit/ci-post.sh
@@ -1,9 +1,17 @@
 #/bin/bash
 
-DROP_DIR=~/webserver/drop
+if [ ! -d "$WEBSERVER_DROPDIR" ]; then
+  WEBSERVER_DROPDIR=~/webserver/drop
+  if [ ! -d "$WEBSERVER_DROPDIR" ]; then
+    echo WEBSERVER_DROPDIR: $WEBSERVER_DROPDIR
+    echo Directory is not valid
+    exit 1
+  fi
+fi
+
 VERSION=`grep -m 1 "version" pom.xml | sed 's/^[^>]*>\(.*\?\)<.*$/\1/'`
 
 # Build Javadoc archive and publish to web staging
 tar cfz target/javadoc.tgz --transform="s,apidocs,helpers/junit/$VERSION/javadoc," -C target apidocs
-cp target/javadoc.tgz $DROP_DIR/helpers-junit.tgz
-echo "rm -r helpers/junit/$VERSION/javadoc" > $DROP_DIR/helpers-junit.tgz.action
+cp target/javadoc.tgz $WEBSERVER_DROPDIR/helpers-junit.tgz
+echo "rm -r helpers/junit/$VERSION/javadoc" > $WEBSERVER_DROPDIR/helpers-junit.tgz.action
